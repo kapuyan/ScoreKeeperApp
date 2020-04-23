b7126c2d1b9fd77ad2eb120ebb9be70850992cf9
kapuyan <kevin.apuyan@enhanceit.us>
2020-04-23 03:21:10 -0400

    Task 4

--- app/src/main/java/com/example/scorekeeper/MainActivity.kt
+++ app/src/main/java/com/example/scorekeeper/MainActivity.kt
@@ -2,8 +2,12 @@ package com.example.scorekeeper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatDelegate
 import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
@@ -12,7 +16,44 @@ class MainActivity : AppCompatActivity() {
     private var mScore2: Int = 0
     lateinit var mScoreText1:TextView
     lateinit var mScoreText2:TextView
+    final var STATE_SCORE_1: String ="Team 1 score"
+    final var STATE_SCORE_2:String ="Team 2 Score"
 
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putInt(STATE_SCORE_1, mScore1);
+        outState.putInt(STATE_SCORE_2, mScore2);
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu);
+        val nightMode = AppCompatDelegate.getDefaultNightMode()
+        if (nightMode==AppCompatDelegate.MODE_NIGHT_YES){
+            menu?.findItem(R.id.night_mode)?.setTitle(R.string.day_mode)
+        }
+        else {
+            menu?.findItem(R.id.night_mode)?.setTitle(R.string.night_mode)
+        }
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId ==R.id.night_mode){
+            val nightMode = AppCompatDelegate.getDefaultNightMode()
+            if (nightMode==AppCompatDelegate.MODE_NIGHT_YES){
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+            else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+            recreate()
+
+        }
+
+        return true;
+        return super.onOptionsItemSelected(item)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +63,15 @@ class MainActivity : AppCompatActivity() {
         mScoreText1 = findViewById<TextView>(R.id.score_1) as TextView
         mScoreText2 = findViewById<TextView>(R.id.score_2) as TextView
 
+        if (savedInstanceState != null) {
+            mScore1 = savedInstanceState.getInt(STATE_SCORE_1);
+            mScore2 = savedInstanceState.getInt(STATE_SCORE_2);
+
+            //Set the score text views
+            mScoreText1.text = mScore1.toString()
+            mScoreText2.text = mScore2.toString()
+          //  mScoreText2.setText(String.valueOf(mScore2));
+        }
     }
 
     fun decreaseScore(view: View) {
